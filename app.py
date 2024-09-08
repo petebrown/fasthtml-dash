0d839a563df97fa08d19d53e36a9b56b77c8187a
Pete Brown <15838790+petebrown@users.noreply.github.com>
2024-09-07 20:25:19 -0400

    added dropdown

---
+++ app.py
@@ -0,0 +1,159 @@
+from fasthtml.common import *
+from trfc_data.h2h_all import *
+from trfc_data.player_apps import *
+
+def df_to_html(df, table_id=None):
+    return Div(
+        Table(
+            Thead(Tr(*[Th(col) for col in df.columns])),
+            Tbody(*[Tr(*[Td(row[col]) for col in df.columns]) for idx, row in df.iterrows()])
+        ),
+        id=table_id
+    )
+
+def df_to_html_expanded(df, table_id=None):
+    if 'game_date' in df.columns:
+        df['game_date'] = df['game_date'].dt.strftime('%d/%m/%Y')
+        df['game_date'] = df.apply(lambda row: A(f"{row['game_date']}", cls="btn btn-primary", href=f"#{row.name}", data_bs_toggle="collapse", role="button", aria_expanded="false", aria_controls=row.name), axis=1)
+    return Div(
+        Table(
+            Thead(Tr(*[Th(col) for col in df.columns])),  # Table headers
+            Tbody(
+                *[
+                    (Tr(*[Td(row[col]) for col in df.columns]),  # Match details
+                    Tr(Td(Div(f"Additional content for {row.game_date}"), colspan=len(df.columns)), cls="collapse", id=idx))  # Line-ups and table
+                    for idx, row in df.iterrows()
+                ]
+            )
+        ),
+        id=table_id
+    )
+
+def page_length_options(df, page_length=20):
+    n_records = len(df)
+    page_length_options = list(range(page_length, n_records, page_length))
+    if n_records % page_length != 0:
+        page_length_options.append(n_records)
+    return [Option(str(o), value=str(o)) for o in page_length_options]
+
+def filter_season(df, season):
+    return df[df.season == season]
+
+def get_game_dates(df=all_results(), reverse=True):
+    return sorted(df.game_date.unique(), reverse=reverse)
+
+def get_game_date_options(game_dates=get_game_dates()):
+    return [Option(str(game_date), value=str(game_date)) for game_date in game_dates]
+
+def get_season_list(reverse=True):
+    return sorted(all_results().season.unique(), reverse=reverse)
+
+def get_season_options(reverse=True):
+    seasons = get_season_list(reverse)
+    return [Option(str(season), value=str(season)) for season in seasons]
+
+cdn = 'https://cdn.jsdelivr.net/npm/bootstrap'
+bootstrap_links = [
+    Link(href=cdn+"@5.3.3/dist/css/bootstrap.min.css", rel="stylesheet"),
+    Script(src=cdn+"@5.3.3/dist/js/bootstrap.bundle.min.js"),
+    Link(href=cdn+"-icons@1.11.3/font/bootstrap-icons.min.css", rel="stylesheet")
+]
+
+app = FastHTML(hdrs=bootstrap_links)
+
+@app.get("/")
+def home():
+    return Title("Links"), Div(
+        H1('Links'),
+        A("Link to Results page", href="/results"),
+        Br(),
+        A("Link to Line Up page", href="/line_ups"),
+        Br(),
+        A("Link to Head to Head page", href="/h2h"),
+        cls='container')
+
+@app.get("/results")
+def results():
+    df = all_results().fillna('-')
+    df = filter_season(df, max(df.season))
+    tab = df_to_html_expanded(df, 'results_table')
+
+    season_options = get_season_options()
+    return Title("Results"), Div(
+        Form(
+            Select(
+                *season_options, # Create a list of Option elements
+                cls='form-select', # Add class 'form-select' to the select element
+                id='season' # Add id 'season' to the select element
+            ),
+            Button('Submit'),
+            hx_post='/season', # Send selected season to @app.post('/season')
+            hx_target='#results_table' # Send response from @app.post('/season') to element with id='results_table'
+        ),
+        H1('Results'),
+        Div(tab, id='results_table'),
+        cls='container')
+
+@app.post('/season')
+async def season_handler(request):
+    season = await request.form()
+    season = season.get('season')
+    df = all_results()[all_results().season == season].fillna('-')
+
+    return df_to_html_expanded(df)
+
+@app.get("/line_ups")
+def lineup():
+    game_date_options = get_game_date_options()
+    return Title('Line-up'), Div(
+        H1('Line-up'),
+        Form(
+            Select(
+                *game_date_options, # Create a list of Option elements
+                cls='form-select', # Add class 'form-select' to the select element
+                id='game_date' # Add id 'game_date' to the select element
+            ),
+            Button('Submit'),
+            hx_post='/line_up', # Send selected game_date to @app.post('/line_up')
+            hx_target='#line_up' # Send response from @app.post('/page_length') to element with id='line_up'
+        ),
+        Div(df_to_html(filter_game(player_apps_df(), max(get_game_dates()))), id='line_up'), # Table that will be updated by @app.post('/line_up')
+        cls='container')
+
+@app.post('/line_up')
+async def line_up_handler(request):
+    game_date = await request.form()
+    game_date = game_date.get('game_date')
+    df = filter_game(player_apps_df(), game_date)
+
+    return df_to_html(df)
+
+@app.get("/h2h")
+def h2h():
+    df = h2h_all(all_results()).fillna('-')
+    tab = df_to_html(df, 'h2h_table')
+    page_lengths = page_length_options(h2h_all(all_results()))
+    return Title("Head to Head Overview"), Div(
+        H1('Head to Head Overview'),
+        Form(
+            Select(
+                *page_lengths, # Create a list of Option elements
+                cls='form-select', # Add class 'form-select' to the select element
+                id='page_length' # Add id 'page_length' to the select element
+            ),
+            Button('Submit'),
+            hx_post='/page_length', # Send selected page length to @app.post('/page_length')
+            hx_target='#h2h_table' # Send response from @app.post('/page_length') to element with id='h2h_table'
+        ),
+        Div(tab, id='h2h_table'), # Table that will be updated by @app.post('/page_length')
+        cls='container')
+
+@app.post('/page_length')
+async def page_length_handler(request):
+    page_length = await request.form()
+    page_length = page_length.get('page_length')
+    df = h2h_all(all_results())[:int(page_length)].fillna('-')
+
+    return df_to_html(df)
+
+serve()
